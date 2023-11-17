43591954a88617afb750da239221da70253cf7d7
Fabian Aviles <95595753+faviles11@users.noreply.github.com>
2023-11-16 21:38:17 -0600

    zoom feature
    zoom feature

--- Proyecto 2 Estructuras/mavenproject12/src/main/java/com/mycompany/mavenproject12/MazePanel.java
+++ Proyecto 2 Estructuras/mavenproject12/src/main/java/com/mycompany/mavenproject12/MazePanel.java
@@ -12,28 +12,28 @@ import java.awt.Color;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.util.Stack;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class MazePanel extends JPanel {
+
     private Maze maze;
-  // private boolean showSolution = true;
-   private boolean showSolution;
-   private Cell currentCell;
-   private boolean navigable = false;
-   
-   
-   
+    // private boolean showSolution = true;
+    private boolean showSolution;
+    private Cell currentCell;
+    private boolean navigable = false;
+
+    private double scale = 1.0;
+
     public MazePanel(Maze maze) {
         this.maze = maze;
-        
+
         this.currentCell = maze.getCells()[0][0]; // o cualquier celda de inicio que desees
 
     }
 
-
-    
     public void setShowSolution(boolean show) {
         showSolution = show;
         repaint(); // Esto asegura que el panel se redibuje con la solución visible o no
@@ -41,8 +41,14 @@ public class MazePanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-       
-        
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        int w = getWidth();
+        int h = getHeight();
+        g2.translate(w / 2, h / 2);
+        g2.scale(scale, scale);
+        g2.translate(-w / 2, -h / 2);
+
         super.paintComponent(g);
         System.out.println("prueba del repaint");
         int panelWidth = getWidth();
@@ -72,7 +78,7 @@ public class MazePanel extends JPanel {
                 }
             }
         }
-        
+
         if (showSolution) {
             System.out.println("Estamos en el SHOW SOLUTION");
             // Si la variable showSolution es verdadera, dibuja la solución
@@ -80,29 +86,25 @@ public class MazePanel extends JPanel {
             maze.resetVisited();
             Stack<Cell> solution = maze.solveMaze();
             g.setColor(Color.BLACK); // Elige el color para la solución
-        
-        for (Cell cell : solution) {
-            System.out.println("Dibujando EN EL FOR  DE la solución");
-            int x = cell.getCol() * cellSize + cellSize / 2;
-            int y = cell.getRow() * cellSize + cellSize / 2;
-            int ovalSize = Math.max(1, cellSize / 4); // Ajusta el tamaño del círculo
-            g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
 
-            System.out.println("x:"+x);
-            System.out.println("y:" +y);
+            for (Cell cell : solution) {
+                System.out.println("Dibujando EN EL FOR  DE la solución");
+                int x = cell.getCol() * cellSize + cellSize / 2;
+                int y = cell.getRow() * cellSize + cellSize / 2;
+                int ovalSize = Math.max(1, cellSize / 4); // Ajusta el tamaño del círculo
+                g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
 
-            
-       }
-         //Cell start = solution.get(0); // Asumiendo que la solución comienza con la celda inicial.
-         //dibujarPuntoEspecial(g, start, Color.GREEN);       
-        
+                System.out.println("x:" + x);
+                System.out.println("y:" + y);
 
-    }
-    
-        else{
+            }
+            //Cell start = solution.get(0); // Asumiendo que la solución comienza con la celda inicial.
+            //dibujarPuntoEspecial(g, start, Color.GREEN);       
+
+        } else {
             System.out.println("NOOO Dibujando la solución");
         }
-    /*
+        /*
     if (!currentSolutionSteps.isEmpty()) {
         g.setColor(Color.RED);
         for (Cell cell : currentSolutionSteps) {
@@ -113,45 +115,60 @@ public class MazePanel extends JPanel {
             g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
         }
     }*/
-     if (navigable && currentCell != null) {
-       
+        if (navigable && currentCell != null) {
 
-        // Calcula las coordenadas del centro de la celda actual
-        int x = currentCell.getCol() * cellSize + cellSize / 2;
-        int y = currentCell.getRow() * cellSize + cellSize / 2;
-        int ovalSize = Math.max(1, cellSize / 4); // Ajusta el tamaño del círculo si es necesario
+            // Calcula las coordenadas del centro de la celda actual
+            int x = currentCell.getCol() * cellSize + cellSize / 2;
+            int y = currentCell.getRow() * cellSize + cellSize / 2;
+            int ovalSize = Math.max(1, cellSize / 4); // Ajusta el tamaño del círculo si es necesario
 
-        g.setColor(Color.RED);
-        g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
-    }
+            g.setColor(Color.RED);
+            g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
+        }
     }
 
     @Override
     public Dimension getPreferredSize() {
-        // Aquí puedes definir el tamaño preferido basándote en el tamaño del laberinto
-        return new Dimension(maze.getCols() * 10, maze.getRows() * 10); // Sustituye 10 por el tamaño deseado de cada celda
+        int w = maze.getCols() * 10;
+        int h = maze.getRows() * 10;
+        return new Dimension((int) (w * scale), (int) (h * scale));
     }
     
-    
-    
-    
-     public void setNavigable(boolean navigable) {
+    public double getScale() {
+       return scale;
+   }
+   public void setScale(double scale) {
+       this.scale = scale;
+       revalidate();
+       repaint();
+   }
+
+    public void setNavigable(boolean navigable) {
         this.navigable = navigable;
         setFocusable(navigable);
         if (navigable) {
             addKeyListener(new KeyAdapter() {
                 @Override
                 public void keyPressed(KeyEvent e) {
-                    if (!navigable) return; // Si no es navegable, no haga nada.
-
+                    if (!navigable) {
+                        return; // Si no es navegable, no haga nada.
+                    }
                     int newRow = currentCell.getRow();
                     int newCol = currentCell.getCol();
 
                     switch (e.getKeyCode()) {
-                        case KeyEvent.VK_UP:    newRow--; break;
-                        case KeyEvent.VK_DOWN:  newRow++; break;
-                        case KeyEvent.VK_LEFT:  newCol--; break;
-                        case KeyEvent.VK_RIGHT: newCol++; break;
+                        case KeyEvent.VK_UP:
+                            newRow--;
+                            break;
+                        case KeyEvent.VK_DOWN:
+                            newRow++;
+                            break;
+                        case KeyEvent.VK_LEFT:
+                            newCol--;
+                            break;
+                        case KeyEvent.VK_RIGHT:
+                            newCol++;
+                            break;
                         // puedes agregar más casos si necesitas otras teclas
                     }
 
@@ -165,37 +182,31 @@ public class MazePanel extends JPanel {
         }
         repaint();
     }
-    
-    
-    
-    
-    
+
     private void moveCurrentCell(int rowDelta, int colDelta) {
-    int newRow = currentCell.getRow() + rowDelta;
-    int newCol = currentCell.getCol() + colDelta;
+        int newRow = currentCell.getRow() + rowDelta;
+        int newCol = currentCell.getCol() + colDelta;
 
-    // Obtiene la nueva celda potencial del laberinto
-    Cell newCell = maze.getCells()[newRow][newCol];
-    
-    // Comprueba si se puede mover a la nueva celda (no hay paredes entre ellas)
-    if (!currentCell.hasWallBetween(newCell)) {
-        currentCell = newCell; // Actualiza la celda actual
-        repaint(); // Repintar el laberinto con la nueva posición
-    }
-}
-
-private boolean canMoveToCell(int newRow, int newCol) {
-    // Comprueba si las nuevas coordenadas están dentro de los límites del laberinto
-    if (newRow >= 0 && newRow < maze.getRows() && newCol >= 0 && newCol < maze.getCols()) {
-        // Obtén la nueva celda basándote en las coordenadas proporcionadas
+        // Obtiene la nueva celda potencial del laberinto
         Cell newCell = maze.getCells()[newRow][newCol];
 
-        // Utiliza el método hasWallBetween() para verificar si hay paredes entre las celdas
-        return !currentCell.hasWallBetween(newCell);
+        // Comprueba si se puede mover a la nueva celda (no hay paredes entre ellas)
+        if (!currentCell.hasWallBetween(newCell)) {
+            currentCell = newCell; // Actualiza la celda actual
+            repaint(); // Repintar el laberinto con la nueva posición
+        }
     }
-    return false; // Las coordenadas están fuera de los límites del laberinto
-}
 
-    
-    
+    private boolean canMoveToCell(int newRow, int newCol) {
+        // Comprueba si las nuevas coordenadas están dentro de los límites del laberinto
+        if (newRow >= 0 && newRow < maze.getRows() && newCol >= 0 && newCol < maze.getCols()) {
+            // Obtén la nueva celda basándote en las coordenadas proporcionadas
+            Cell newCell = maze.getCells()[newRow][newCol];
+
+            // Utiliza el método hasWallBetween() para verificar si hay paredes entre las celdas
+            return !currentCell.hasWallBetween(newCell);
+        }
+        return false; // Las coordenadas están fuera de los límites del laberinto
+    }
+
 }
--- Proyecto 2 Estructuras/mavenproject12/src/main/java/com/mycompany/mavenproject12/MazeWindow.java
+++ Proyecto 2 Estructuras/mavenproject12/src/main/java/com/mycompany/mavenproject12/MazeWindow.java
@@ -4,114 +4,116 @@
  */
 package com.mycompany.mavenproject12;
 
-/**
- *
- * @author esteb
- */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Stack;
 
-
-
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
-import java.util.Stack;
-import java.util.Iterator;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.table.DefaultTableModel;
-import java.util.Random;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class MazeWindow extends JFrame {
 
-    private MazePanel mazePanel;
-    private Maze maze;
-    private JButton toggleSolutionButton;
-    private boolean showSolution = true;
-    private boolean navigable = false;
-    private JButton navigateMazeButton; // Botón para activar la navegación
+  private MazePanel mazePanel;
+  private Maze maze;
+  private JButton toggleSolutionButton;
+  private boolean showSolution = true;
+  private boolean navigable = false;
+  private JButton navigateMazeButton; // Botón para activar la navegación
 
+  private JScrollPane scrollPane;
 
-    
-    public MazeWindow(Maze maze) {
-        this.maze = maze;
+  private JButton zoomInButton;
+  private JButton zoomOutButton;
 
-        // Configuración inicial de la ventana
-        setTitle("Laberinto");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+  public MazeWindow(Maze maze) {
 
-        // Crear el panel del laberinto y añadirlo a la ventana
-        mazePanel = new MazePanel(maze);
-        add(mazePanel);
+      this.maze = maze;
+      mazePanel = new MazePanel(maze);
 
-        // Crear el botón de guardar y su ActionListener
-        JButton saveButton = new JButton("MAZE WINDOW Guardar Laberinto");
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setDialogTitle("Guardar laberinto como XML");
-                int userSelection = fileChooser.showSaveDialog(MazeWindow.this);
+      // Configuración inicial de la ventana
+      setTitle("Laberinto");
+      setSize(800, 600);
+      setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-                if (userSelection == JFileChooser.APPROVE_OPTION) {
-                    File fileToSave = fileChooser.getSelectedFile();
-                    String filePath = fileToSave.getAbsolutePath();
-                    // Asegúrate de que el archivo tenga la extensión .xml
-                    if (!filePath.endsWith(".xml")) {
-                        filePath += ".xml";
-                    }
-                    maze.saveMazeToXml(filePath);
+      // Crear el botón de guardar y su ActionListener
+      JButton saveButton = new JButton("MAZE WINDOW Guardar Laberinto");
+      saveButton.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              JFileChooser fileChooser = new JFileChooser();
+              fileChooser.setDialogTitle("Guardar laberinto como XML");
+              int userSelection = fileChooser.showSaveDialog(MazeWindow.this);
+
+              if (userSelection == JFileChooser.APPROVE_OPTION) {
+                File fileToSave = fileChooser.getSelectedFile();
+                String filePath = fileToSave.getAbsolutePath();
+                // Asegúrate de que el archivo tenga la extensión .xml
+                if (!filePath.endsWith(".xml")) {
+                    filePath += ".xml";
                 }
-            }
-        });
-        navigateMazeButton = new JButton("Navigate Maze");
-        navigateMazeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                navigable = !navigable; // Cambia el estado de navegación
-                navigateMazeButton.setText(navigable ? "Disable Navigation" : "Enable Navigation");
-                mazePanel.setNavigable(navigable); // Actualiza el panel con el nuevo estado
-                if (navigable) {
-                    mazePanel.requestFocusInWindow(); // Solicita el foco para capturar los eventos de teclado
-                }
-            }
-        });
-        
-        
+                maze.saveMazeToXml(filePath);
+              }
+          }
+      });
+      navigateMazeButton = new JButton("Navigate Maze");
+      navigateMazeButton.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              navigable = !navigable; // Cambia el estado de navegación
+              navigateMazeButton.setText(navigable ? "Disable Navigation" : "Enable Navigation");
+              mazePanel.setNavigable(navigable); // Actualiza el panel con el nuevo estado
+              if (navigable) {
+                mazePanel.requestFocusInWindow(); // Solicita el foco para capturar los eventos de teclado
+              }
+          }
+      });
 
-    // Añadir el botón de navegación al panel de botones
-    
-        
-       
-       toggleSolutionButton = new JButton("Show Solution");
-       toggleSolutionButton.addActionListener(new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        showSolution = !showSolution; // Cambia el estado de la solución
-        toggleSolutionButton.setText(showSolution ? "Hide Solution" : "Show Solution");
-        mazePanel.setShowSolution(showSolution); // Actualiza el panel con el nuevo estado
-    }
-});
-       
+      // Añadir el botón de navegación al panel de botones
+      toggleSolutionButton = new JButton("Show Solution");
+      toggleSolutionButton.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              showSolution = !showSolution; // Cambia el estado de la solución
+              toggleSolutionButton.setText(showSolution ? "Hide Solution" : "Show Solution");
+              mazePanel.setShowSolution(showSolution); // Actualiza el panel con el nuevo estado
+          }
+      });
 
-        // Añadir el botón de guardar a la ventana
-        JPanel buttonPanel = new JPanel(); // Puedes utilizar un JPanel para ordenar tus botones
-        buttonPanel.add(saveButton);
-        buttonPanel.add(toggleSolutionButton );
-        //buttonPanel.add(navigateMazeButton);
-        buttonPanel.add(navigateMazeButton);
-        this.getContentPane().add(buttonPanel, BorderLayout.SOUTH); // Añadir el botón en la parte sur del layout
-    }
+      // Añadir el botón de guardar a la ventana
+      JPanel buttonPanel = new JPanel(); // Puedes utilizar un JPanel para ordenar tus botones
+      buttonPanel.add(saveButton);
+      buttonPanel.add(toggleSolutionButton);
+      //buttonPanel.add(navigateMazeButton);
+      buttonPanel.add(navigateMazeButton);
+      this.getContentPane().add(buttonPanel, BorderLayout.SOUTH); // Añadir el botón en la parte sur del layout
 
-  
+      zoomInButton = new JButton("Zoom In");
+      zoomInButton.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              mazePanel.setScale(mazePanel.getScale() * 1.1);
+          }
+      });
+      zoomOutButton = new JButton("Zoom Out");
+      zoomOutButton.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              mazePanel.setScale(mazePanel.getScale() / 1.1);
+          }
+      });
+
+      JPanel zoomPanel = new JPanel();
+      zoomPanel.add(zoomInButton);
+      zoomPanel.add(zoomOutButton);
+
+      JPanel centerPanel = new JPanel(new BorderLayout());
+      centerPanel.add(mazePanel, BorderLayout.CENTER);
+      centerPanel.add(zoomPanel, BorderLayout.SOUTH);
+
+      this.getContentPane().add(centerPanel, BorderLayout.CENTER);
+  }
+
 }
+
+
