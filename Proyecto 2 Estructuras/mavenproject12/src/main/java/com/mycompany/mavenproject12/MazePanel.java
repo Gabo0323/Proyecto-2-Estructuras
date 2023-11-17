133d173418dd7ee8b9d9fed300e017a19a5ff769
FranciscoArias9 <104183291+FranciscoArias9@users.noreply.github.com>
2023-11-17 10:37:40 -0600

    Update MazePanel.java

--- Proyecto 2 Estructuras/mavenproject12/src/main/java/com/mycompany/mavenproject12/MazePanel.java
+++ Proyecto 2 Estructuras/mavenproject12/src/main/java/com/mycompany/mavenproject12/MazePanel.java
@@ -4,10 +4,6 @@
  */
 package com.mycompany.mavenproject12;
 
-/**
- *
- * @author esteb
- */
 import java.awt.Color;
 import javax.swing.JPanel;
 import java.awt.Graphics;
@@ -16,6 +12,9 @@ import java.awt.Graphics2D;
 import java.util.Stack;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.BasicStroke;
+import java.awt.FontMetrics;
+import java.awt.Font;
 
 public class MazePanel extends JPanel {
 
@@ -24,6 +23,7 @@ public class MazePanel extends JPanel {
     private boolean showSolution;
     private Cell currentCell;
     private boolean navigable = false;
+     private boolean usePaintComponent2 = false;
 
     private double scale = 1.0;
 
@@ -42,7 +42,11 @@ public class MazePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
+        if(usePaintComponent2){
+            paintComponent2(g);
+        }
+        else{
+            Graphics2D g2 = (Graphics2D) g;
         int w = getWidth();
         int h = getHeight();
         g2.translate(w / 2, h / 2);
@@ -125,7 +129,128 @@ public class MazePanel extends JPanel {
             g.setColor(Color.RED);
             g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
         }
+        
+        
+        if (currentCell.equals(maze.getCells()[maze.getRows() - 1][maze.getCols() - 1])) {
+            String message = "¡Felicidades, has completado el laberinto!";
+            g.setColor(Color.GREEN);
+            g.setFont(new Font("Arial", Font.BOLD, 20));
+
+            FontMetrics metrics = g.getFontMetrics();
+            int x = (getWidth() - metrics.stringWidth(message)) / 2;
+            int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
+
+            g.drawString(message, x, y);
+        }
+        }
     }
+    
+    
+    
+    protected void paintComponent2(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2 = (Graphics2D) g;
+    int w = getWidth();
+    int h = getHeight();
+    g2.translate(w / 2, h / 2);
+    g2.scale(scale, scale);
+    g2.translate(-w / 2, -h / 2);
+
+    // Configuración del color y grosor de las paredes del laberinto
+    g2.setColor(Color.ORANGE); // Color naranja para las paredes
+    float thickness = 3.0f; // Grosor de las paredes
+    g2.setStroke(new BasicStroke(thickness));
+
+    int panelWidth = getWidth();
+    int panelHeight = getHeight();
+    int cellWidth = panelWidth / maze.getCols();
+    int cellHeight = panelHeight / maze.getRows();
+    int cellSize = Math.min(cellWidth, cellHeight);
+
+    // Draw the maze
+    Cell[][] cells = maze.getCells();
+    for (int r = 0; r < cells.length; r++) {
+        for (int c = 0; c < cells[r].length; c++) {
+            Cell cell = cells[r][c];
+            int x = c * cellSize;
+            int y = r * cellSize;
+            if (cell.hasTopWall()) {
+                g2.drawLine(x, y, x + cellSize, y);
+            }
+            if (cell.hasLeftWall()) {
+                g2.drawLine(x, y, x, y + cellSize);
+            }
+            if (cell.hasBottomWall()) {
+                g2.drawLine(x, y + cellSize, x + cellSize, y + cellSize);
+            }
+            if (cell.hasRightWall()) {
+                g2.drawLine(x + cellSize, y, x + cellSize, y + cellSize);
+            }
+        }
+    }
+    if (showSolution) {
+            System.out.println("Estamos en el SHOW SOLUTION");
+            // Si la variable showSolution es verdadera, dibuja la solución
+            //Stack<Cell> solution = maze.solveMaze();
+            maze.resetVisited();
+            Stack<Cell> solution = maze.solveMaze();
+            g.setColor(Color.BLACK); // Elige el color para la solución
+
+            for (Cell cell : solution) {
+                System.out.println("Dibujando EN EL FOR  DE la solución");
+                int x = cell.getCol() * cellSize + cellSize / 2;
+                int y = cell.getRow() * cellSize + cellSize / 2;
+                int ovalSize = Math.max(1, cellSize / 4); // Ajusta el tamaño del círculo
+                g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
+
+                System.out.println("x:" + x);
+                System.out.println("y:" + y);
+
+            }
+            //Cell start = solution.get(0); // Asumiendo que la solución comienza con la celda inicial.
+            //dibujarPuntoEspecial(g, start, Color.GREEN);       
+
+        } else {
+            System.out.println("NOOO Dibujando la solución");
+        }
+        /*
+    if (!currentSolutionSteps.isEmpty()) {
+        g.setColor(Color.RED);
+        for (Cell cell : currentSolutionSteps) {
+            // Dibuja cada paso de la solución
+            int x = cell.getCol() * cellSize + cellSize / 2;
+            int y = cell.getRow() * cellSize + cellSize / 2;
+            int ovalSize = Math.max(1, cellSize / 4);
+            g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
+        }
+    }*/
+        if (navigable && currentCell != null) {
+
+            // Calcula las coordenadas del centro de la celda actual
+            int x = currentCell.getCol() * cellSize + cellSize / 2;
+            int y = currentCell.getRow() * cellSize + cellSize / 2;
+            int ovalSize = Math.max(1, cellSize / 4); // Ajusta el tamaño del círculo si es necesario
+
+            g.setColor(Color.RED);
+            g.fillOval(x - ovalSize / 2, y - ovalSize / 2, ovalSize, ovalSize);
+        }
+        
+        
+        
+        if (currentCell.equals(maze.getCells()[maze.getRows() - 1][maze.getCols() - 1])) {
+            String message = "¡Felicidades, has completado el laberinto!";
+            g.setColor(Color.GREEN);
+            g.setFont(new Font("Arial", Font.BOLD, 20));
+
+            FontMetrics metrics = g.getFontMetrics();
+            int x = (getWidth() - metrics.stringWidth(message)) / 2;
+            int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
+
+            g.drawString(message, x, y);
+        }
+
+    // El resto del código permanece igual...
+}
 
     @Override
     public Dimension getPreferredSize() {
@@ -142,6 +267,15 @@ public class MazePanel extends JPanel {
        revalidate();
        repaint();
    }
+   
+   public void setUsePaintComponent2(boolean use) {
+    usePaintComponent2 = use;
+    repaint();
+}
+
+public boolean isUsingPaintComponent2() {
+    return usePaintComponent2;
+}
 
     public void setNavigable(boolean navigable) {
         this.navigable = navigable;
